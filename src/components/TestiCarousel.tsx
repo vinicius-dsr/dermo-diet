8b327acd221ae754ea86419d7de24c1cf9d75562
unknown <viniciusreis3105@gmail.com>
2024-11-26 18:47:14 -0300

    feat: add carousel componet with react and change style for mobile

---
+++ src/components/TestiCarousel.tsx
@@ -0,0 +1,72 @@
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "./ui/carousel";
+
+export default function TestiCarousel() {
+	const testimonials = [
+		{
+			name: "Testemunho",
+			image: "/test.jpeg",
+		},
+		{
+			name: "Testemunho 1",
+			image: "/test-1.jpeg",
+		},
+		{
+			name: "Testemunho 3",
+			image: "/test-3.jpeg",
+		},
+		{
+			name: "Testemunho 4",
+			image: "/test-4.jpeg",
+		},
+		{
+			name: "Testemunho 5",
+			image: "/test-5.jpeg",
+		},
+		{
+			name: "Testemunho 6",
+			image: "/test-6.jpeg",
+		},
+		{
+			name: "Testemunho 9",
+			image: "/test-9.jpeg",
+		},
+		{
+			name: "Testemunho 11",
+			image: "/test-11.jpeg",
+		},
+		{
+			name: "Testemunho 13",
+			image: "/test-13.jpeg",
+		},
+	];
+	return (
+		<Carousel
+			opts={{
+				align: "start",
+			}}
+			className="w-full"
+		>
+			<CarouselContent className="">
+				{testimonials.map((testimonial) => (
+					<CarouselItem key={testimonial.name} className="md:basis-1/3">
+						<div className="rounded-md md:h-auto h-[650px] w-[90%] md:w-full mx-auto">
+							<img
+								src={testimonial.image}
+								alt={testimonial.name}
+								className="w-full h-full object-cover"
+							/>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselPrevious className="hidden md:flex" />
+			<CarouselNext className="hidden md:flex" />
+		</Carousel>
+	);
+}
